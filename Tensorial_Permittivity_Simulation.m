1a5792f93dd1b32642eff5b6b4f57f4788e3d696
Alice Calvert <dallas6vert@gmail.com>
2026-07-29 21:20:20 -0400

    Add files via upload

--- Tensorial_Permittivity_Simulation.m
+++ Tensorial_Permittivity_Simulation.m
@@ -1,10 +1,11 @@
-%% Last updated: 2026-07-21 by Alice Calvert
-%% This is a program to calculate the dielectric permittivity tensor of a magneto-optic (gyrotropic) anisotropic material.
-%% The inputs are the core size (b), shell thickness (d), and planar number density [1/m^2] of the nanoparticles, as well as the magnetic flux density (B).
-%% The outputs are the components εxx and εxy of the permittivity tensor at each wavelength. For εzz, run the function again with B=0. [1]
-%% The simulation is adapted from the Absorption Simulation function by Kenzie Lewis and Raaja Rajeshwari Manickam, based off algorithm by Dani et al. [3]
+%% Last updated: 2026-07-29 by Alice Calvert
+%% This is a function to calculate the dielectric permittivity tensor of a magneto-optic (gyrotropic) anisotropic material using Maxwell-Garnett theory.
+%% The inputs are the nanoparticle radius (b) [nm] and material, magnitude of magnetic flux density (B) [T] applied to nanoparticles, and the range of wavelengths.
+%% The outputs are the components εxx and εxy of the permittivity tensor at each wavelength. In the weak field limit, ωB·τ << 1, where ωB is cyclotron 
+%% frequency and τ is a characteristic relaxation time defined in the Drude model, the diagonal permittivity tensor component εzz ≃ εxx [1].
+%% The simulation is adapted from the Absorption Simulation function by Kenzie Lewis and Raaja Rajeshwari Manickam, based off algorithm by Dani et al. [2]
 %% Make sure fitted parameters are up to date with the most recent experimental data.
-%% All units are SI except the absorption coefficient (cm^-1). Angles are in rads.
+%% All units are SI.
 
 %% -------------------------------------------------------------------------- %%
 %% ------------------------------- References ------------------------------- %%
@@ -20,10 +21,9 @@
 %% -------------------------------------------------------------------------- %%
 %% ------------------------------ EMA Function ------------------------------ %%
 %% -------------------------------------------------------------------------- %%
-function [wavelength,eps_XX_lambda,eps_XY_lambda]=Tensorial_Permittivity_Simulation(core_radius,shell_thickness,planar_density,magnetic_flux) 
+function [wavelength,eps_XX_lambda,eps_XY_lambda]=Tensorial_Permittivity_Simulation(particle_radius,material,magnetic_flux,wavelength) 
 
-%The program will starting timing the run.
-%tic
+tic %start timing run
 
 %% General Parameters & Constants
 
@@ -33,46 +33,61 @@ kb=1.38064852e-23;                 % Boltzmann constant
 me=9.10938356e-31;                 % effective mass of electron
 e=1.60217662e-19;                  % elementary charge
 mu0=4*pi*1e-7;                     % vacuum permeability
-Bz=magnetic_flux;                  % applied magnetic field along z [T] 
 B=magnetic_flux;                   % applied magnetic field [T] 
 
-b=core_radius*1e-9;                % core radius  
-d=shell_thickness*1e-9;            % shell thickness
-a=b+d;                             % particle radius
+b=particle_radius*1e-9;            % nanoparticle radius  
 c_Vs=(4/3)*pi*b^3;                 % core volume
-ns=planar_density/(2*a);           % number density
-Vs=(4/3)*pi*a^3;                   % particle volume
-fs=ns*Vs;                          % volume fraction of particles in medium
 
-wavelength = 200e-9:1e-9:900e-9;     % sweep (comment out fixed wavelength)
-% wavelength = 532e-9;               % fixed (comment out wavelength sweep)
 eps_XX_lambda = zeros(length(wavelength),1);
 eps_XY_lambda = zeros(length(wavelength),1);
 
 %% ------------ Parameters of magneto-optically active material ------------- %%
 
-%% Option 1: SnO2 (comment out Fe2O3 parameters) [3]
+if strcmpi(material, 'sno2')
+    %% Option 1: SnO2 [3]
+    Ms=250e3;                                   % saturation magnetization, 250-300e3 [3]
+    c_g0=1.2e15;                                % fitted parameter for tin oxide absorption 
+    c_w0=6.7e15;                                % fitted parameter for tin oxide absorption 
+    c_gamma0=9e15;                              % fitted parameter for tin oxide absorption  
 
-Keff=5e3;                                   % effective anisotropy constant, 5-9e3 [3]
-Ms=250e3;                                   % saturation magnetization, 250-300e3 [3]
-c_wp=0;                                     % plasma frequency
-vf =1.4e6;                                  % Fermi velocity of gold
-c_gammap = (2.75e14 / (0.347e-15)) + vf / b;        % damping frequency, CHANGE
-c_g0=1.2e15;                                % fitted parameter for tin oxide absorption 
-c_w0=6.7e15;                                % fitted parameter for tin oxide absorption 
-c_gamma0=9e15;                              % fitted parameter for tin oxide absorption  
+elseif strcmpi(material, 'fe2o3')
+    %% Option 2: Fe2O3 [2]
+    Ms=414e3;%250e3;                          % saturation magnetization, 250-300e3
+    c_g0=5.2e15;                              % fitted parameter for iron oxide absorption [2]
+    c_w0=5.06e15;                             % fitted parameter for iron oxide absorption [2]
+    c_gamma0=2.89e15;                         % fitted parameter for iron oxide absorption [2]
 
-%% Option 2: Fe2O3 (comment out SnO2 parameters) [2]
+elseif strcmpi(material, 'other')
+    %% Option 3: Other materials
 
-% Keff=4700;%9e3;                           % effective anisotropy constant, 5-9e3
-% Ms=414e3;%250e3;                          % saturation magnetization, 250-300e3
-% c_wp=0;                                   % plasma frequency
-% c_gammap=1/(0.347e-15)+vf/b;              % damping frequency
-% c_g0=5.2e15;                              % fitted parameter for iron oxide absorption [2]
-% c_w0=5.06e15;                             % fitted parameter for iron oxide absorption [2]
-% c_gamma0=2.89e15;                         % fitted parameter for iron oxide absorption [2]
+    confirmed = false;
+    while ~confirmed
 
-%% All Options (do not comment out)
+        Ms = input('Enter the saturation magnetization [A/m]:');
+        c_g0 = input('Enter the oscillator strength of bound electrons:');
+        c_w0 = input('Enter the binding frequency [Hz] of bound electrons:');
+        c_gamma0 = input('Enter the damping frequency [Hz] of bound electrons:');
+        
+        while true
+
+            should_continue = input('Proceed with entered values? ("yes" or "no"):','s');
+    
+            if strcmpi(should_continue, 'yes')
+                confirmed = true;
+                break;
+            elseif strcmpi(should_continue, 'no')
+                fprintf('Proceeding to re-enter values.\n\n')
+                break;
+            else
+                fprintf('Invalid input. Please enter "yes" or "no".\n\n');
+            end
+
+        end
+
+    end
+
+end
+
 Bzint=(((2/9)*mu0*c_Vs*Ms^2)/(kb*T))*B;     % internal magnetic field
 c_wB=(e*Bzint)/(me);                        % cyclotron frequency, assuming bulk effective mass of 9.5me^2 
 
@@ -93,7 +108,8 @@ for i = 1:length(wavelength)
     eps_XX_lambda(i) = eps_XX;
     eps_XY_lambda(i) = eps_XY;
 
-    %The program will stop timing the run
-    %toc 
 end
+
+toc %stop timing run
+
 end
